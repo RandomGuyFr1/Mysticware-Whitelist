450652ef520628983a68219acb7e30a67dcc9807
RandomGuyFr1 <147245928+RandomGuyFr1@users.noreply.github.com>
2023-10-07 17:55:01 +0700

    Update and rename whitelisted (1).lua to whitelisted.lua

---
+++ whitelisted.lua
@@ -0,0 +1,40 @@
+
+
+
+{
+  "RayHafz": [
+    {"id": "5071878111"},
+    {"id": "11"},
+    {"id": "11"}
+],
+  "Private": [
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"}
+],
+  "tags": [
+    {"userid": "5071878111", "tag": "RAYFEM", "color": "pink"},
+    {"userid": "1", "tag": "nn", "color": "red"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "18", "tag": "nn", "color": "yellow"},
+    {"userid": "19", "tag": "nn", "color": "cyan"},
+    {"userid": "20", "tag": "nn", "color": "purple"},
+    {"userid": "21", "tag": "nn", "color": "pink"},
+    {"userid": "22", "tag": "nn", "color": "purple"},
+    {"userid": "23", "tag": "nn", "color": "purple"},
+    {"userid": "24", "tag": "nn", "color": "red"}
+  ]
+}
